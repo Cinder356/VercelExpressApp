e5ba2fa9943afd814e3c86b33812075eeee82794
Daniil <dga03@mail.ru>
2024-10-28 22:06:43 +0600

    with pug

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import Express from "express"
 
 const app = Express();
+app.use(Express.static("public"));
+app.set("view engine", "pug");
 
-app.get('/', (req, res) => res.send("Hi"));
+app.get('/', (req, res) => {
+    const currentTime = new Date();
+    const timeString = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+    const data = { title: "recieving time", time: timeString };
+    res.render("index", data);
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
