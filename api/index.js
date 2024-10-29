7ed40e1528fec51cb0e29ebde2853d75deadca87
Daniil <dga03@mail.ru>
2024-10-29 12:43:17 +0600

    .pug

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     const currentTime = new Date();
     const timeString = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
     const data = { title: "recieving time", time: timeString };
-    res.render("index", data);
+    res.render("index.pug", data);
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
