67bd1069db01caf10af4fa9d01a7923c3e626230
Daniil <dga03@mail.ru>
2024-10-30 18:24:51 +0600

    views pathway

--- api/index.js
+++ api/index.js
@@ -5,11 +5,13 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const viewsPathway = path.join(__dirname, '../views');
+const staticPathway = path.join(__dirname, '../public');
+
 const app = Express();
 app.use(Express.static("public"));
 app.set("view engine", "pug");
-app.set("views", "views");
-const staticPathway = path.join(__dirname, '../public');
+app.set("views", viewsPathway);
 app.use('/assets', Express.static(staticPathway)) // например http://localhost:3000/assets/css/index.css . то есть мы типо к assets обращаемся
 
 app.get('/', (req, res) => {
